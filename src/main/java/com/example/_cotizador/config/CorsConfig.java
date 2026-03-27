894e04041d678cf11ff07d00d7eb25fd4cadac26
ngvp-nuve <ni.villagrap@duocuc.cl>
2026-03-27 14:49:53 -0400

    update

--- src/main/java/com/example/_cotizador/config/CorsConfig.java
+++ src/main/java/com/example/_cotizador/config/CorsConfig.java
@@ -3,15 +3,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite acceso a TODAS las URLs
-                .allowedOrigins("http://localhost:4200") // Permite a Angular (puerto 4200)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite estos métodos
-                .allowedHeaders("*")
-                .allowCredentials(true);
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("http://cotizador.mejoratusalud.cl")
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
